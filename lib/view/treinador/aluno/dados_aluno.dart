ec5056360e25cdf00dbbb80f9ebd53008b8f45db
Thiago <othiagosilva.dev@gmail.com>
2021-10-14 20:02:53 -0300

    att 0.15

--- lib/view/treinador/aluno/dados_aluno.dart
+++ lib/view/treinador/aluno/dados_aluno.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:lib/view/treinador/aluno/dados_aluno.dart
 import 'package:my_trainer/view/usuario.dart';
 
 class Aluno extends Usuario {
@@ -34,40 +33,3 @@ class Aluno extends Usuario {
   var massaGorda;
   var genero;
 }
-=======
-import 'package:my_trainer/view/usuario.dart';
-
-class Aluno extends Usuario {
-  var cod;
-  var nome;
-  var idade;
-  var peso;
-  var altura;
-  var cintura;
-  var quadril;
-  var perimetroAbdomen;
-  var dobraSubEscapular;
-  var dobraTricipital;
-  var dobraPeitoral;
-  var dobraAxilarMedio;
-  var dobraSupraIliaca;
-  var dobraAbdomen;
-  var dobraCoxa;
-  var somaDobra;
-  var perimetroTorax;
-  var perimetroBracoRel;
-  var perimetroBracoCon;
-  var perimetroAntebraco;
-  var perimetroCintura;
-  var perimetroQuadril;
-  var perimetroCoxas;
-  var perimetroPanturrilha;
-  var limitacoes;
-  var imc;
-  var razaoCinturaQuadril;
-  var percentualGordura;
-  var massaMagra;
-  var massaGorda;
-  var genero;
-}
->>>>>>> f79389f196062577a04bd3946dfa4390c6dd2714:lib/dados_aluno.dart
--- lib/widgets/widget_NavButton.dart
+++ lib/widgets/widget_NavButton.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:lib/widgets/widget_NavButton.dart
 import 'package:flutter/material.dart';
 
 class WidgetNavButton extends StatelessWidget {
@@ -40,46 +39,3 @@ class WidgetNavButton extends StatelessWidget {
     );
   }
 }
-=======
-import 'package:flutter/material.dart';
-
-class WidgetNavButton extends StatelessWidget {
-  final rotulo;
-  final proximaPag;
-  final cor;
-  const WidgetNavButton(this.rotulo, this.cor, {this.proximaPag, Key? key})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 8),
-      child: ElevatedButton(
-        child: Text(
-          rotulo,
-          style: TextStyle(
-            fontSize: 24,
-          ),
-        ),
-        style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all<Color>(
-            cor,
-          ),
-          elevation: MaterialStateProperty.all<double>(0),
-          fixedSize: MaterialStateProperty.all<Size>(
-            Size(200, 50),
-          ),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0),
-            ),
-          ),
-        ),
-        onPressed: () {
-          Navigator.pushNamed(context, proximaPag);
-        },
-      ),
-    );
-  }
-}
->>>>>>> f79389f196062577a04bd3946dfa4390c6dd2714:lib/widgets/widget_myButton.dart

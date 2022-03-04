5366d247a4e149b4cbfe7d3d2a9c9bfe70ae4d3a
Thiago Silva <othiagosilva.dev@gmail.com>
2022-03-04 14:39:48 -0300

    extracao do tema

--- lib/main.dart
+++ lib/main.dart
@@ -15,52 +15,17 @@ import 'package:my_trainer/pages/treinos/consultar_treino.dart';
 import 'package:my_trainer/pages/feedback/feedback.dart';
 import 'package:my_trainer/pages/renda/consultar_renda.dart';
 
+import 'components/theme.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
-  final ThemeData theme = ThemeData();
 
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'myTrainer',
-      theme: theme.copyWith(
-        primaryColor: Color.fromRGBO(198, 26, 26, 1),
-        backgroundColor: Color.fromRGBO(17, 17, 17, 1),
-        colorScheme:
-            theme.colorScheme.copyWith(secondary: Color.fromRGBO(71, 9, 9, 1)),
-        textTheme: TextTheme(
-          headline1: TextStyle(
-            fontSize: 24,
-            color: Colors.white,
-            fontFamily: 'OpenSans',
-            fontWeight: FontWeight.bold,
-          ),
-          headline2: TextStyle(
-            fontSize: 24,
-            color: Color.fromRGBO(198, 26, 26, 1),
-            fontWeight: FontWeight.bold,
-          ),
-          headline3: TextStyle(
-            fontSize: 22,
-            color: Colors.white,
-          ),
-          headline4: TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-          headline5: TextStyle(
-            fontSize: 22,
-            color: Colors.black,
-          ),
-          headline6: TextStyle(
-            fontSize: 24,
-            color: Colors.white,
-          ),
-        ),
-      ),
+      theme: myTrainerTheme,
       initialRoute: 'login',
       routes: {
         'login': (context) => LoginPage(),
